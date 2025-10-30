6e0d668377729adca799555bfcb16129dfd8310d
nmbenvino <nmbenvino@comunidad.unnoba.edu.ar>
2025-10-29 21:18:46 -0300

    modificacion de ParticipanteService (paso de class a interface), añadido ParticipanteServiceImp (implementa ParticipanteService)

--- Poo2025/src/main/java/ar/edu/unnoba/poo2025/torneos/service/ParticipanteService.java
+++ Poo2025/src/main/java/ar/edu/unnoba/poo2025/torneos/service/ParticipanteService.java
@@ -1,47 +1,19 @@
 package ar.edu.unnoba.poo2025.torneos.service;
+
 import java.util.List;
 import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import ar.edu.unnoba.poo2025.torneos.model.ParticipanteModel;
-import ar.edu.unnoba.poo2025.torneos.repository.ParticipanteRepository;
 
-@Service
-public class ParticipanteService {
+public interface ParticipanteService {
 
-    @Autowired
-    private ParticipanteRepository participanteRepo;
+    List<ParticipanteModel> obtenerParticipantes();
 
-    public List<ParticipanteModel> obtenerParticipantes() {
-        return participanteRepo.findAll();
-    }
+    Optional<ParticipanteModel> obtenerPorId(Long id);
 
-    public Optional<ParticipanteModel> obtenerPorId(Long id) {
-        return participanteRepo.findById(id);
-    }
+    public void crear(ParticipanteModel participant) throws Exception;
 
-    public ParticipanteModel crear(ParticipanteModel participante) {
-        return participanteRepo.save(participante);
-    }
- 
-    public void eliminar(Long id) {
-        participanteRepo.deleteById(id);
-    }
+    void eliminar(Long id);
 
-    public void actualizar(Long id, ParticipanteModel nuevosDatos) {
-        ParticipanteModel existente = participanteRepo.findById(id).orElse(null);
-        if (existente != null) {
-    
-            existente.setNombre(nuevosDatos.getNombre());
-            existente.setApellido(nuevosDatos.getApellido());
-            existente.setTipoDni(nuevosDatos.getTipoDni());
-            existente.setNumeroDni(nuevosDatos.getNumeroDni());
-            existente.setEmail(nuevosDatos.getEmail());
-            existente.setContraseña(nuevosDatos.getContraseña());
-
-            participanteRepo.save(existente);
-        }
-    }
+    void actualizar(Long id, ParticipanteModel nuevosDatos);
 }
