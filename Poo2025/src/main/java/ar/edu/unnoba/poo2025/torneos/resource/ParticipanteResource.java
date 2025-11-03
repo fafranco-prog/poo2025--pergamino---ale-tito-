42b0cfdb08656e1e9d068cee787c9071cdd9fbda
nmbenvino <nmbenvino@comunidad.unnoba.edu.ar>
2025-11-03 19:21:11 -0300

    añadido el metodo authentication (path /auth, metodo POST)

--- Poo2025/src/main/java/ar/edu/unnoba/poo2025/torneos/resource/ParticipanteResource.java
+++ Poo2025/src/main/java/ar/edu/unnoba/poo2025/torneos/resource/ParticipanteResource.java
@@ -2,6 +2,8 @@ package ar.edu.unnoba.poo2025.torneos.resource;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.Map;
+import java.util.Collections;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.modelmapper.ModelMapper;
@@ -9,8 +11,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import ar.edu.unnoba.poo2025.torneos.dto.AuthenticationRequestDTO;
 import ar.edu.unnoba.poo2025.torneos.model.ParticipanteModel;
 import ar.edu.unnoba.poo2025.torneos.dto.CrearParticipanteRequestDTO;
+import ar.edu.unnoba.poo2025.torneos.service.AuthenticationServiceImp;
 import ar.edu.unnoba.poo2025.torneos.service.ParticipanteServiceImp;
 
 @RestController
@@ -19,9 +23,10 @@ public class ParticipanteResource {
 
     @Autowired
     private ParticipanteServiceImp participanteService;
-
     @Autowired
     private ModelMapper modelMapper;
+    @Autowired
+    private AuthenticationServiceImp authenticationService;
 
     @GetMapping("/getParticipantes")
     public List<ParticipanteModel> getParticipantes() {
@@ -33,8 +38,17 @@ public class ParticipanteResource {
         return participanteService.obtenerPorId(id);
     }
 
-    @PostMapping("/")
+    @DeleteMapping("/delParticipante/{id}")
+    public void eliminarParticipante(@PathVariable Long id) {
+        participanteService.eliminar(id);
+    }
 
+    @PutMapping("/updateParticipante/{id}")
+    public void actualizarParticipante(@PathVariable Long id, @RequestBody ParticipanteModel datosNuevos) {
+        participanteService.actualizar(id, datosNuevos);
+    }
+
+    @PostMapping(path = "/")
     public ResponseEntity<?> crearParticipante(@RequestBody CrearParticipanteRequestDTO participanteDTO) {
         try {
             ParticipanteModel participanteModel = modelMapper.map(participanteDTO, ParticipanteModel.class);
@@ -45,13 +59,15 @@ public class ParticipanteResource {
         }
     }
 
-    @DeleteMapping("/delParticipante/{id}")
-    public void eliminarParticipante(@PathVariable Long id) {
-        participanteService.eliminar(id);
-    }
-
-    @PutMapping("/updateParticipante/{id}")
-    public void actualizarParticipante(@PathVariable Long id, @RequestBody ParticipanteModel datosNuevos) {
-        participanteService.actualizar(id, datosNuevos);
+    @PostMapping(path = "/auth", produces = "application/json")
+    public ResponseEntity<?> authentication(@RequestBody AuthenticationRequestDTO authenticationRequestDTO) {
+        try {
+            ParticipanteModel participanteModel = modelMapper.map(authenticationRequestDTO, ParticipanteModel.class);
+            String token = authenticationService.authenticate(participanteModel);
+            Map<String, String> response = Collections.singletonMap("token", token);
+            return new ResponseEntity<>(response, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.UNAUTHORIZED);
+        }
     }
 }
