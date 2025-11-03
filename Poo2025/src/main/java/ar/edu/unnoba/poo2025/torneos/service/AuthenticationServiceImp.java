141a03823ae779a0f452034c871dc40a81f46535
nmbenvino <nmbenvino@comunidad.unnoba.edu.ar>
2025-11-03 19:19:29 -0300

    añadido AuthenticationService (interfaz) y AuthenticationServiceImp (clase que implementa la interfaz)

---
+++ Poo2025/src/main/java/ar/edu/unnoba/poo2025/torneos/service/AuthenticationServiceImp.java
@@ -0,0 +1,31 @@
+package ar.edu.unnoba.poo2025.torneos.service;
+
+import ar.edu.unnoba.poo2025.torneos.model.ParticipanteModel;
+import ar.edu.unnoba.poo2025.torneos.util.JwtTokenUtil;
+import ar.edu.unnoba.poo2025.torneos.util.PasswordEncoder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticationServiceImp implements AuthenticationService {
+
+    @Autowired
+    private ParticipanteService participanteService;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+    @Autowired
+    private JwtTokenUtil jwtTokenUtil;
+
+    @Override
+    public String authenticate(ParticipanteModel participante) throws Exception {
+        ParticipanteModel participanteGuardado = participanteService.obtenerPorEmail(participante.getEmail());
+        if (participanteGuardado == null) {
+            throw new Exception("El email no existe.");
+        }
+        if (!passwordEncoder.verificar(participante.getContraseña(), participanteGuardado.getContraseña())) {
+            throw new Exception("Contraseña incorrecta.");
+        }
+
+        return jwtTokenUtil.generarToken(participanteGuardado.getEmail());
+    }
+}
