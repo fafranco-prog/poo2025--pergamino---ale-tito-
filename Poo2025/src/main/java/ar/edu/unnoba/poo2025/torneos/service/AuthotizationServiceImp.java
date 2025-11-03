868ffcd4b7bd87b0f734e77b8e7d5beb3579481f
nmbenvino <nmbenvino@comunidad.unnoba.edu.ar>
2025-11-03 19:19:56 -0300

    añadido AuthorizationService (interfaz) y AuthorizationServiceImp (clase que implementa la interfaz)

---
+++ Poo2025/src/main/java/ar/edu/unnoba/poo2025/torneos/service/AuthotizationServiceImp.java
@@ -0,0 +1,28 @@
+package ar.edu.unnoba.poo2025.torneos.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import ar.edu.unnoba.poo2025.torneos.model.ParticipanteModel;
+import ar.edu.unnoba.poo2025.torneos.util.JwtTokenUtil;
+
+public class AuthotizationServiceImp implements AuthorizationService {
+
+    @Autowired
+    private JwtTokenUtil jwtTokenUtil;
+    @Autowired
+    private ParticipanteService participanteService;
+
+    @Override
+    public ParticipanteModel authorize(String token) throws Exception {
+
+        if (jwtTokenUtil.validarToken(token)) {
+            throw new Exception("El token no es valido");
+        }
+        String email = jwtTokenUtil.getSubject(token);
+        ParticipanteModel participante = participanteService.obtenerPorEmail(email);
+        if (participante == null) {
+            throw new Exception("El email no existe.");
+        }
+        return participante;
+    }
+}
