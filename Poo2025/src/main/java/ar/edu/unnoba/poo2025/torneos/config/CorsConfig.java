9d2baef7787684e3dcf2055da75c836fe1cb46b1
nmbenvino <nmbenvino@comunidad.unnoba.edu.ar>
2026-01-19 01:57:04 -0300

    Se añadio un archivo de configuracion de CORS

---
+++ Poo2025/src/main/java/ar/edu/unnoba/poo2025/torneos/config/CorsConfig.java
@@ -0,0 +1,18 @@
+package ar.edu.unnoba.poo2025.torneos.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
+                .allowedHeaders("Authorization", "Content-Type", "Accept", "Origin", "X-Requested-With", "Access-Control-Request-Method", "Access-Control-Request-Headers")
+                .allowCredentials(true);
+    }
+}
