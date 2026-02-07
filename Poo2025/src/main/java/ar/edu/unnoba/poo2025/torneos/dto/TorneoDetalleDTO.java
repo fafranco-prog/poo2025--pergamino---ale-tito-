0535d05dd172b852ab864f104e8673f1b9124f26
nmbenvino <nmbenvino@comunidad.unnoba.edu.ar>
2026-02-07 17:34:30 -0300

    Se añadio la descripcion al dto, por requerimiento del front

--- Poo2025/src/main/java/ar/edu/unnoba/poo2025/torneos/dto/TorneoDetalleDTO.java
+++ Poo2025/src/main/java/ar/edu/unnoba/poo2025/torneos/dto/TorneoDetalleDTO.java
@@ -1,12 +1,10 @@
 package ar.edu.unnoba.poo2025.torneos.dto;
 
-
-
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
 import lombok.Getter;
-import lombok.Setter; 
+import lombok.Setter;
 
 @Getter
 @Setter
@@ -14,10 +12,10 @@ public class TorneoDetalleDTO {
 
     private Long id;
     private String nombre;
+    private String descripcion;
     private boolean publicado;
     private int totalInscripciones;
     private BigDecimal montoTotal;
     private LocalDate fechaFin;
     private LocalDate fechaIni;
 }
- 
